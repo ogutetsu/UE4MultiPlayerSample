c2f9248d495368674dee26e73dafe018a4de1c5a
og <ogugu@test.com>
2020-01-20 11:29:52 +0900

    [update] OnlineSessionの取得を追加

--- Source/MultiPlayerSample/MyGameInstance.cpp
+++ Source/MultiPlayerSample/MyGameInstance.cpp
@@ -33,6 +33,11 @@ void UMyGameInstance::Init()
 	if(Subsystem != nullptr)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Found subsystem %s"), *Subsystem->GetSubsystemName().ToString());
+		IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+		if (SessionInterface.IsValid())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Found session interface %s"), *Subsystem->GetSubsystemName().ToString());
+		}
 	}
 	else
 	{
