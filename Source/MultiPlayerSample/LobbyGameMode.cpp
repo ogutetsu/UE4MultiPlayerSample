830c3793c859e1eb9714431e4669a56c40748059
og <ogugu@test.com>
2020-01-21 18:17:05 +0900

    [add] Lobby用のGameModeを追加

---
+++ Source/MultiPlayerSample/LobbyGameMode.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	++NumberOfPlayers;
+
+	if (NumberOfPlayers >= 3)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Reached 3 players!"));
+	}
+}
+
+void ALobbyGameMode::Logout(AController* Exiting)
+{
+	--NumberOfPlayers;
+}
---
+++ Source/MultiPlayerSample/LobbyGameMode.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MultiPlayerSampleGameMode.h"
+#include "LobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSAMPLE_API ALobbyGameMode : public AMultiPlayerSampleGameMode
+{
+	GENERATED_BODY()
+
+public:
+
+	void PostLogin(APlayerController* NewPlayer) override;
+
+	void Logout(AController* Exiting) override;
+
+private:
+	uint32 NumberOfPlayers = 0;
+
+
+};
