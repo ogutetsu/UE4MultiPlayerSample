f3fc26dc9b3a5c3062810765885a67e7eb78a0be
og <ogugu@test.com>
2020-01-22 17:41:54 +0900

    [update] Move構造体をキューに追加

--- Source/MultiPlayerSample/Karts/GoKart.cpp
+++ Source/MultiPlayerSample/Karts/GoKart.cpp
@@ -59,11 +59,14 @@ void AGoKart::Tick(float DeltaTime)
 
 	if (IsLocallyControlled())
 	{
-		FGoKartMove Move;
-		Move.DeltaTime = DeltaTime;
-		Move.SteeringThrow = SteeringThrow;
-		Move.Throttle = Throttle;
+		FGoKartMove Move = CreateMove(DeltaTime);
+		if (!HasAuthority())
+		{
+			UnacknowledgedMoves.Add(Move);
+			UE_LOG(LogTemp, Warning, TEXT("Queue length: %d"), UnacknowledgedMoves.Num());
+		}
 
+		
 		Server_SendMove(Move);
 		SimulateMove(Move);
 	}
@@ -98,6 +101,8 @@ void AGoKart::OnRep_ServerState()
 {
 	SetActorTransform(ServerState.Transform);
 	Velocity = ServerState.Velocity;
+
+	ClearAcknowledgeMoves(ServerState.LastMove);
 }
 
 void AGoKart::SimulateMove(FGoKartMove Move)
@@ -114,6 +119,30 @@ void AGoKart::SimulateMove(FGoKartMove Move)
 
 
 	UpdateLocationFromVelocity(Move.DeltaTime);
+}
+
+FGoKartMove AGoKart::CreateMove(float DeltaTime)
+{
+	FGoKartMove Move;
+	Move.DeltaTime = DeltaTime;
+	Move.SteeringThrow = SteeringThrow;
+	Move.Throttle = Throttle;
+	Move.Time = GetWorld()->TimeSeconds;
+	return Move;
+}
+
+void AGoKart::ClearAcknowledgeMoves(FGoKartMove LastMove)
+{
+	TArray<FGoKartMove> NewMoves;
+
+	for (const FGoKartMove& Move : UnacknowledgedMoves)
+	{
+		if (Move.Time > LastMove.Time)
+		{
+			NewMoves.Add(Move);
+		}
+	}
+	UnacknowledgedMoves = NewMoves;
 }
 
 FVector AGoKart::GetAirResistance()
--- Source/MultiPlayerSample/Karts/GoKart.h
+++ Source/MultiPlayerSample/Karts/GoKart.h
@@ -71,6 +71,9 @@ public:
 private:
 	void SimulateMove(FGoKartMove Move);
 
+	FGoKartMove CreateMove(float DeltaTime);
+	void ClearAcknowledgeMoves(FGoKartMove LastMove);
+
 	FVector GetAirResistance();
 	FVector GetRollingResistance();
 
@@ -115,5 +118,6 @@ private:
 	float Throttle;
 	float SteeringThrow;
 
+	TArray<FGoKartMove> UnacknowledgedMoves;
 
 };
