b7fa692030a23d64e29604482dabe3f448818dec
og <ogugu@test.com>
2020-01-22 11:14:19 +0900

    [add] Vehicle用のリソースを追加

---
+++ Source/MultiPlayerSample/Karts/KartsGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "KartsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AKartsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AKartsGameMode();
+};
+
+
+
